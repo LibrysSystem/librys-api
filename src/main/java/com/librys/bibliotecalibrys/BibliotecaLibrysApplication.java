f620aad5721bfb80088cf3087f0451db40d3bb2a
juliabre <juliasilvaabreu83@gmail.com>
2024-04-23 15:18:54 -0300

    init

---
+++ src/main/java/com/librys/bibliotecalibrys/BibliotecaLibrysApplication.java
@@ -0,0 +1,13 @@
+package com.librys.bibliotecalibrys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaLibrysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaLibrysApplication.class, args);
+    }
+
+}
