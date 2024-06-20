b0b884a43da802e9866802199981305d84ca6acb
NayraSousa <nayraamaral1@gmail.com>
2024-06-20 10:27:55 -0300

    credenciais funcionando

--- src/main/java/com/librys/bibliotecalibrys/api/controller/UsuarioController.java
+++ src/main/java/com/librys/bibliotecalibrys/api/controller/UsuarioController.java
@@ -1,22 +1,27 @@
 package com.librys.bibliotecalibrys.api.controller;
 
-import com.librys.bibliotecalibrys.domain.model.Funcionario;
-import com.librys.bibliotecalibrys.domain.model.Usuario;
-import com.librys.bibliotecalibrys.domain.repository.UsuarioRepository;
-import com.librys.bibliotecalibrys.domain.service.UsuarioService;
 import com.librys.bibliotecalibrys.security.authentication.AuthenticationService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
-@RequiredArgsConstructor
 public class UsuarioController {
 
     private final AuthenticationService authenticationService;
+
+    public UsuarioController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public String authenticate(Authentication authentication){
+        return authenticationService.authenticate(authentication);
+    }
+
 
     private final UsuarioRepository usuarioRepository;
 
--- src/main/java/com/librys/bibliotecalibrys/security/config/SecurityConfiguration.java
+++ src/main/java/com/librys/bibliotecalibrys/security/config/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,9 +35,7 @@ public class SecurityConfiguration {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers("/usuario/login").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/funcionarios").permitAll()
+                        auth -> auth.requestMatchers("/usuario/login").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .oauth2ResourceServer(
